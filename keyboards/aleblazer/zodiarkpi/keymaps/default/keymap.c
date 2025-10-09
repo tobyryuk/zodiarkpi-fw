844efba5b2894698c619958f58834a261d44ddd9
Toby Ryuk <ryuk.toby@gmail.com>
2025-10-09 09:34:02 +0200

    Update keymap and add via config

--- keyboards/aleblazer/zodiarkpi/keymaps/default/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/default/keymap.c
@@ -13,22 +13,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "keymap_us_international.h"
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT(
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-      MO(3), KC_A,  KC_S,    KC_D,    KC_F,    KC_G,  KC_MINS, KC_MUTE,     RGB_TOG, KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_GRV, KC_LBRC,      KC_RBRC, KC_DEL,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-      KC_LCTL, KC_LALT, KC_LGUI, KC_F12, MO(1),  KC_SPC,   KC_ENT,    KC_ENT,   KC_SPC,  MO(2),    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_GRV, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,  KC_LBRC, KC_MUTE,     RGB_TOG, KC_RBRC,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_GRV, KC_MINS,      KC_EQL, KC_DEL,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_F12, MO(1),  KC_SPC,   MO(1),    MO(2),   KC_ENT,  MO(3),    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
 	[1] = LAYOUT(
       _______, KC_F1, KC_F2, KC_F3, KC_F4,   KC_F5,                                       KC_F6,   KC_F7,   KC_F8,  KC_F9, KC_F10, KC_F11,
-      KC_PSLS, KC_P7, KC_P8, KC_P9, _______, KC_NUM,                                      KC_NUM,  KC_PSLS, KC_P7, KC_P8, KC_P9, KC_F12,
-      KC_CAPS, KC_P4, KC_P5, KC_P6, _______, _______, _______, _______,  _______,  _______, _______, _______, KC_P4, KC_P5, KC_P6, _______,
-      _______, KC_P1, KC_P2, KC_P3, _______, _______, _______, _______,  _______, _______, _______, _______,  KC_P1, KC_P2, KC_P3, _______,
-      _______, KC_P0, KC_PDOT, KC_PENT, _______,     _______,  _______,   _______,    _______,   _______,     KC_P0, KC_PDOT, KC_PENT, _______
+      KC_PSLS, KC_P7, KC_P8, KC_P9, _______, KC_NUM,                                      _______,  _______, _______, _______, _______, US_ARNG,
+      KC_CAPS, KC_P4, KC_P5, KC_P6, _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______, US_ODIA, US_ADIA,
+      _______, KC_P1, KC_P2, KC_P3, _______, _______, _______, _______,  _______, _______, _______, _______,  _______, _______, _______, _______,
+      _______, KC_P0, KC_PDOT, KC_PENT, _______,     _______,  _______,   _______,    _______,   _______,     _______, _______, _______, _______
       ),
 
 	[2] = LAYOUT(
